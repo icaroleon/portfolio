1a40d790d4d4ee5f08122f53433e2c0dc5956058
Icaro Leon <icvieiramg@gmail.com>
2022-07-12 23:53:18 -0300

    Style ok

--- files/js/script.js
+++ files/js/script.js
@@ -5,6 +5,7 @@ function openTab(evt, TabName) {
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
+      
     }
    var i, tabcontent, tablinks;
   
@@ -13,11 +14,15 @@ function openTab(evt, TabName) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace("active", "");
     }
+
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("col-6", "");
+    }
   
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(TabName).style.display = "block";
     evt.currentTarget.className += " active";
-    
+    evt.currentTarget.className += " col-6";
   }
   document.getElementById("defaultOpen").click()
   
