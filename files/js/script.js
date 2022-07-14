2dc6cf7004e15c056808f5be24d6cd8afbc6b566
Icaro Leon <icvieiramg@gmail.com>
2022-07-14 20:52:10 -0300

    refatorado

--- files/js/script.js
+++ files/js/script.js
@@ -1,46 +1,33 @@
 function openTab(evt, TabName) {
-    // Declare all variables
-    var i, tabcontent;
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-      
-    }
-   var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace("active", "");
-    }
-
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace("col-6", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(TabName).style.display = "block";
-    evt.currentTarget.className += " active";
-    evt.currentTarget.className += " col-6";
+  // Declare all variables
+  var i, tabcontent;
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    
   }
-  document.getElementById("defaultOpen").click()
+ var i, tabcontent, tablinks;
 
-  const scrollContainer = document.querySelector("div.projects");
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
 
-scrollContainer.addEventListener("wheel", (evt) => {
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("col-6", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(TabName).style.display = "block";
+  evt.currentTarget.className += " active";
+  evt.currentTarget.className += " col-6";
+}
+document.getElementById("defaultOpen").click()
+
+const scrollContainer = document.querySelector("div.projects");
+  scrollContainer.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
-    console.log('deu')
 });
-
-  /* const scrollContainer = document.getElementsByClassName("horizontal-scrollable");
-
-    scrollContainer[0].addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-    console.log('Ok')
-  }); */
-  
-
-  
