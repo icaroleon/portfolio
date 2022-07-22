6322f453cdb6b8484fb0603383a09ca9b84cadd9
Icaro Leon <icvieiramg@gmail.com>
2022-07-22 15:22:13 -0300

    mudanca de escopo

--- files/js/script.js
+++ files/js/script.js
@@ -1,11 +1,17 @@
+document.getElementById("defaultOpen").click()
+
 function openTab(evt, TabName) {
   // Declare all variables
   var i, tabcontent;
   // Get all elements with class="tabcontent" and hide them
+
+  infocontent = document.getElementById("infoContent")
+
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
-    
+    //infocontent.style.display = "none";
+
   }
  var i, tabcontent, tablinks;
 
@@ -24,10 +30,38 @@ function openTab(evt, TabName) {
   evt.currentTarget.className += " active";
   evt.currentTarget.className += " col-6";
 }
-document.getElementById("defaultOpen").click()
 
-const scrollContainer = document.querySelector("div.projects");
-  scrollContainer.addEventListener("wheel", (evt) => {
+//scroll projects with the mouse wheel
+const scrollContainerProjects = document.querySelector("div.projects");
+scrollContainerProjects.addEventListener("wheel", (evt) => {
     evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainerProjects.scrollLeft += evt.deltaY;
 }); 
+
+const scrollContainerAboutMe = document.querySelector("div.aboutMe");
+  scrollContainerAboutMe.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainerAboutMe.scrollLeft += evt.deltaY;
+}); 
+
+//hide cv icon and show text download as soon the mouse enter the button
+document.getElementsByClassName('cv-text-button')[1].style.display = 'none'
+function showDownloadText(evt, TextName) {
+  document.getElementsByClassName('cv-text-button')[1].style.display="inline-block"
+  
+  const buttonCV = document.getElementsByClassName('cv-text-button');
+  for (i = 0; i < buttonCV.length; i++) {
+    buttonCV[1].style.display = "inline-block";
+  }
+}
+
+//show cv icon and hide text download as soon the mouse outs the button
+function hideDownloadText(evt, TextName) {
+  
+  const buttonCV = document.getElementsByClassName('cv-text-button')
+    for (i = 0; i < buttonCV.length; i++) {
+      buttonCV[0].style.display = "inline-block";
+      buttonCV[1].style.display = "none";
+    }
+}
+
